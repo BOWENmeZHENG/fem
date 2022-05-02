5bf9d240911b7573c5bddc1068d323515282f195
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-05-02 15:23:39 -0700

    run whole process

--- parametrised/fem.py
+++ parametrised/fem.py
@@ -16,4 +16,34 @@ def meshing(pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4, mesh_size=0.008):
             geom.add_polygon(pts_inner)
         )
         mesh = geom.generate_mesh()
-    return mesh.points
+    return mesh
+
+
+def write_files(mesh, E=1.0e8, nu=0.3, load=-20.0e3):
+    mesh_pts = mesh.points
+    elements = mesh.cells_dict['triangle']
+
+    # Write nodes.txt
+    # fix bottom
+    with open('nodes.txt', 'w') as f_nodes:
+        for i, point in enumerate(mesh_pts):
+            if point[1] == min(mesh_pts[:, 1]):
+                bc_x, bc_y = -1, -1
+            else:
+                bc_x, bc_y = 0, 0
+            f_nodes.write(f"{i:4} {point[0]:8.4f} {point[1]:8.4f}  {bc_x:4}  {bc_y:4} \n")
+
+    # Write eles.txt
+    with open('eles.txt', 'w') as f_eles:
+        for i, element in enumerate(elements):
+            f_eles.write(f"{i:4}   3   0  {element[0]:4} {element[1]:4}  {element[2]:4} \n")
+
+    # Write mater.txt
+    with open('mater.txt', 'w') as f_mater:
+        f_mater.write(f"{E:8.4f} {nu:8.4f}")
+
+    # Write mater.txt
+    with open('loads.txt', 'w') as f_loads:
+        for i, point in enumerate(mesh_pts):
+            if point[1] == max(mesh_pts[:, 1]):
+                f_loads.write(f"{i:4} {0.0:8.4f} {load:8.4f} \n")
--- parametrised/main.py
+++ parametrised/main.py
@@ -1,8 +1,16 @@
 from geometry import define_pts as pts
 import graphing
 import fem
+from solidspy import solids_GUI
+import matplotlib.pyplot as plt
 
 pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4 = pts()
-mesh_points = fem.meshing(pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4)
-graphing.scatter_nodes(mesh_points)
+mesh = fem.meshing(pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4)
+fem.write_files(mesh)
+
+disp = solids_GUI(folder="")  # run the Finite Element Analysis
+plt.show()    # plot contours
+
+
+# graphing.scatter_nodes(mesh.points)
 # graphing.plot_parts([pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4])
