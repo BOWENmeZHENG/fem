38e082206df34e7879498bba9cd1512896c3af86
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2022-05-03 09:53:07 -0700

    calc_pr

--- parametrised/run_fem.py
+++ parametrised/run_fem.py
@@ -15,7 +15,16 @@ def run(theta, alpha, size=0.2, thickness=0.02, extension=0.25):
 
     disp = solids_GUI(folder="")  # run the Finite Element Analysis
     plt.show()  # plot contours
-    return disp
+    return disp, mesh
 
+
+def calc_pr(disp, mesh, extension=0.25):
+    id_up, id_right, id_left = fem.nodes_interested(mesh)
+    disp_axial = disp[id_up, 1]
+    disp_lateral = disp[id_right, 0] - disp[id_left, 0]
+    strain_axial = disp_axial / (extension * 2)
+    strain_lateral = disp_lateral / (extension * 2)
+    pr = -strain_lateral / strain_axial
+    return pr
 # graphing.scatter_nodes(mesh.points)
 # graphing.plot_parts([pts_outer, pts_inner, ext_1, ext_2, ext_3, ext_4, inside_up, inside_down])
